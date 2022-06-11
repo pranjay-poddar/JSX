7738a6aef659dca5d454a305be1812e05918cd76
pranjay-poddar <pranjaypoddar@outlook.com>
2022-06-12 01:53:50 +0530

    project initialized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+function namereturn(user){
+return user.firstName + ' ' + user.lastName;
+}
+
+ const user = {
+  firstName : "Pranjay",
+  lastName : "Poddar" 
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="blank">{namereturn(user)}</div>
+    <br/>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+
+    <h1 className="heading">This Page is Coded By Pranjay Poddar</h1>
+
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+      Molestias expedita maiores quidem hic inventore animi odio, ullam neque! Soluta,
+      exercitationem officia reprehenderit porro fugit enim sit iusto iure quos veniam?</p>    
+    </>
+     
   );
 }
 

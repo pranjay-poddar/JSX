465400d88bdb3e1da896c26f878f0aed015c9e60
pranjay-poddar <pranjaypoddar@outlook.com>
2022-06-12 02:34:10 +0530

    added nav

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import './App.css';
-function namereturn(user){
-return user.firstName + ' ' + user.lastName;
-}
-
- const user = {
-  firstName : "Pranjay",
-  lastName : "Poddar" 
-};
-
 function App() {
   return (
     <>
-    <div className="blank">{namereturn(user)}</div>
-    <br/>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-
-    <h1 className="heading">This Page is Coded By Pranjay Poddar</h1>
-
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-      Molestias expedita maiores quidem hic inventore animi odio, ullam neque! Soluta,
-      exercitationem officia reprehenderit porro fugit enim sit iusto iure quos veniam?</p>    
-    </>
-     
-  );
+ <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand " href="/">Text Utilities</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Link</a>
+        </li>
+ 
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </> 
+);
 }
-
 export default App;
